d4f6bc0ec320f612b917f020ae3c88b7e4a75716
VictorHugoMA <victor.alves1@ufu.br>
2021-03-08 22:07:49 -0300

    add

--- Lista3_ED1/ex10.c
+++ Lista3_ED1/ex10.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-#include<stdbool.h>
 //10 - Operadores boleanos(logicos)
 
     int main(){
         int a, b;
-        bool x=false, y=false;
 
         printf("==Operadores Boleanos==\n");
         printf("Digite 0 para falso e nao 0 para verdadeiro\n");
@@ -12,26 +10,20 @@
         scanf("%d", &a);
         printf("Entre com o segundo valor (B): ");
         scanf("%d", &b);
-
-        if(a!=0)
-            x=true;
-        if(b!=0)
-            y=true;
-
-
-        if(x==false || y==false)
+   
+        if(a==0 || b==0)
             printf("A and B: FALSO\n");
             else 
                 printf("A and B: VERDADEIRO\n");
-        if(x!=false || y!=false)
+        if(a!=0 || b!=0)
             printf("A or B: VERDADEIRO\n");
             else
                 printf("A or B: FALSO\n");
-        if(x==false && y==false || x!=false && y!=false)
+        if(a==0 && b==0 || a!=0 && b!=0)
             printf("A xor B: FALSO\n");
             else
                 printf("A xor B: VERDADEIRO\n");
-        if(x==false)
+        if(a==0)
             printf("not A: VERDADEIRO\n");
             else 
                 printf("not A: FALSO\n");    
