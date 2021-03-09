663a9ea86619115eaa20641fcb1fb9402b19feda
VictorHugoMA <victor.alves1@ufu.br>
2021-03-09 15:02:13 -0300

    add

--- Lista5_ED1/ex6.c
+++ Lista5_ED1/ex6.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-//5 - 5 valores
+//6 - 5 valores com indice
 
     int main(){
-        int vet[5], maior, menor, soma=0, i;
+        int vet[5], maior, menor, ima=0, ime=0, soma=0, i;
         float media;
 
         printf("<<5 valores>>\n");
@@ -18,12 +18,15 @@
         for(i=0; i<5; i++){
             soma+=vet[i];
 
-            if(vet[i]>maior)
+            if(vet[i]>maior){
                 maior=vet[i];
+                ima=i;
+            }
 
-            if(vet[i]<menor)
+            if(vet[i]<menor){
                 menor=vet[i];
-
+                ime=i;
+            }
         }
             media=soma/5.0;
 
@@ -31,7 +34,7 @@
             for(i=0; i<5; i++){
                 printf("%d ", vet[i]);
             }
-            printf("\nO maior valor e: %d\n", maior);
-            printf("O menor valor e: %d\n", menor);
+            printf("\nO maior valor e: %d, localizado na posicao %d do vetor\n", maior, ima);
+            printf("O menor valor e: %d, localizado na posicao %d do vetor\n", menor, ime);
             printf("A media e: %.1f\n", media);
     }
