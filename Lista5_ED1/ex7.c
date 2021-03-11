06cc6fbb21707bacd7a68c1f70421d730c66dcc7
VictorHugoMA <victor.alves1@ufu.br>
2021-03-11 15:56:39 -0300

    add

--- Lista5_ED1/ex7.c
+++ Lista5_ED1/ex7.c
@@ -2,6 +2,21 @@
 //7 - Notas normalizadas
 
     int main(){
-        int notas[5];
+        int i;
+        float nota[5], maior=0, r;
         
+        printf("<<Normalizando as notas>>\n");
+        for(i=0; i<5; i++){
+            printf("Entre com a nota do aluno %d: ", i+1);
+            scanf("%f", &nota[i]);
+            if(nota[i]>maior)
+                maior=nota[i];
+        }
+
+        r=100/maior;
+
+        printf("\nNotas normalizadas\n");
+        for(i=0; i<5; i++){
+            printf("A nota do aluno %d e %.0f\n", i+1, nota[i]*r);
+        }
     }
