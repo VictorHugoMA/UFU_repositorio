4f9e259b95cdc4b009ed8fb83f24e7f90c5bc725
VictorHugoMA <victor.alves1@ufu.br>
2021-03-31 11:01:38 -0300

    formatado

---
+++ Listas_ED1/Lista12_ED1/ex21.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+//21 - Multiplicacao de vetor Funcao
+
+    void multvet(int *vet, int n, int escalar){
+        for(int i=0; i<n; i++){
+            vet[i]*=escalar;
+        }
+    }
+
+    int main(){
+        int i, *p, n, esc;
+
+        printf("Digite o tamanho do vetor: ");
+        scanf("%d", &n);
+
+        p=(int *)malloc(n*sizeof(int));
+
+        for(i=0; i<n; i++){
+            printf("Digite o valor %d: ", i+1);
+            scanf("%d", &p[i]);
+        }
+        printf("Digite o valor do escalar: ");
+        scanf("%d", &esc);
+
+        printf("\n");
+        printf("O vetor de origem eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %d,", p[i]);
+        }
+        printf(" %d", p[i]);
+
+        multvet(p, n, esc);
+
+        printf("\n");
+        printf("O vetor final eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %d,", p[i]);
+        }
+        printf(" %d", p[i]);
+
+
+    }
---
+++ Listas_ED1/Lista12_ED1/ex22.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+//22 - Valor absoluto Funcao
+
+    void abs_vet(int *vet, int n){
+        for(int i=0; i<n; i++){
+            vet[i]=abs(vet[i]);
+        }
+    }
+
+    int main(){
+        int i, *p, n;
+
+        printf("Digite o tamanho do vetor: ");
+        scanf("%d", &n);
+
+        p=(int *)malloc(n*sizeof(int));
+
+        for(i=0; i<n; i++){
+            printf("Digite o valor %d: ", i+1);
+            scanf("%d", &p[i]);
+        }
+
+        printf("\n");
+        printf("O vetor de origem eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %d,", p[i]);
+        }
+        printf(" %d", p[i]);
+
+        abs_vet(p, n);
+
+        printf("\n");
+        printf("O vetor com valores absolutos eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %d,", p[i]);
+        }
+        printf(" %d", p[i]);
+    }
