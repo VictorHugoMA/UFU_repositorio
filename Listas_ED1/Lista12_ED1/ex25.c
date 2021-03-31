7d8e25434e54d4d0c4a6fda363496245d18250c1
VictorHugoMA <victor.alves1@ufu.br>
2021-03-31 16:55:25 -0300

    formatado

---
+++ Listas_ED1/Lista12_ED1/ex25.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+//25 - Copia vetor, aloc dentro Funcao
+
+    double *copiarvet(double *vet, int n){
+        int i;
+        double *p;
+
+        p=(double *)malloc(n*sizeof(double));
+
+        for(i=0; i<n; i++){
+            p[i]=vet[i];
+        }
+        return p;
+    }
+
+    int main(){
+        int i, n=5;
+        double vet_origem[5]={1.2,2.0,3.2,4.0,5.1}, *vet_destino;
+
+        vet_destino=copiarvet(vet_origem, n);
+
+        printf("O vetor de origem eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %.2lf,", vet_origem[i]);
+        }
+        printf(" %.2lf\n", vet_origem[i]);
+
+        printf("O vetor de destino eh:");
+        for(i=0; i<n-1; i++){
+            printf(" %.2lf,", vet_destino[i]);
+        }
+        printf(" %.2lf\n", vet_destino[i]);
+
+        free(vet_destino);
+
+    }
---
+++ Listas_ED1/Lista12_ED1/ex27.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+//27 - Pontos com aloc dinamica Funcao
+
+    typedef struct ponto{
+        int x, y;
+    }ponto;
+
+    ponto *npontos(int n){
+        ponto *p;
+
+        p=(ponto *)calloc(n, sizeof(ponto));
+
+        return p;
+
+    }
+    
+    int main(){
+        int i, n;
+        ponto *pt;
+
+        printf("Digite quantos ponto(x,y) voce deseja: ");
+        scanf("%d", &n);
+
+        pt=npontos(n);
+
+        printf("(%d,%d) ,", pt[0].x, pt[0].y);
+        for(i=1; i<n-1; i++){
+            printf(" (%d,%d) ,", pt[i].x, pt[i].y);
+        }
+        printf(" (%d,%d)", pt[i].x, pt[i].y);
+
+
+        free(pt);
+    }
