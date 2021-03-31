708de11677b0574dbe19b150934cb8b29ced506a
VictorHugoMA <victor.alves1@ufu.br>
2021-03-31 16:19:56 -0300

    formatado

---
+++ Listas_ED1/Lista12_ED1/ex24.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+//24 - Alocar Funcao
+
+    int *aloca_inteiro(int n){
+        int *p;
+        p=(int *)calloc(n,sizeof(int));
+
+        return p;
+    }
+    void imprime_vet_int(int *vetori, int n){
+        int i;
+        for(i=0; i<n-1; i++){
+            printf(" %d,", vetori[i]);
+        }
+            printf(" %d", vetori[i]);
+    }
+
+    int main(){
+        int *p, n;
+
+        printf("Digite o tamanho do vetor: ");
+        scanf("%d", &n);
+
+        p=aloca_inteiro(n);
+
+        printf("O vetor eh:");
+        imprime_vet_int(p, n);
+
+        free(p);
+    }
