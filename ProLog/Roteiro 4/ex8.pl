5c9f30bcf573deead220d48df0cb516bdacf6332
VictorHugoMA <victor.alves1@ufu.br>
2021-04-01 16:30:00 -0300

    add

--- ProLog/Roteiro 4/ex8.pl
+++ ProLog/Roteiro 4/ex8.pl
@@ -1,3 +1,10 @@
+%a
+disjunto(X, []).
+disjunto([], X).
+
+disjunto([H|T], [H1|TS]):-
+    disjunto([H|T], TS),
+    disjunto(T, [H1|TS]).
 
 
 
@@ -7,4 +14,16 @@ uniao([], [], []).
 uniao(L, K, M):-
     append(L, K, M).
 
-%n
+
+
+%d
+diferenca([], _, []).
+
+diferenca([H|T], [H|TS], Y):-
+    diferenca(T, TS, Y).
+
+diferenca([H|T], [_|TS], [H|Y]):-
+    diferenca(T, TS, Y).
+
+
+
