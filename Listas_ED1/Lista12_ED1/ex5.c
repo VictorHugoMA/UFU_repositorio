0ba77bf9f066fa215467ef6063660895d2fd232e
VictorHugoMA <victor.alves1@ufu.br>
2021-04-03 16:09:02 -0300

    formatado

--- Listas_ED1/Lista12_ED1/ex5.c
+++ Listas_ED1/Lista12_ED1/ex5.c
@@ -16,11 +16,11 @@
     int main(){
         int num;
 
-        printf("Numero: ");
+        printf("Digite o valor de n: ");
         scanf("%d", &num);
 
         if(quadPerf(num))
-            printf("Eh quadrado perfeito\n");
+            printf("O numero %d eh um quadrado perfeito", num);
         else
-            printf("Nao eh quadrado perfeito\n");
+            printf("O numero %d nao eh um quadrado perfeito", num);
     }
