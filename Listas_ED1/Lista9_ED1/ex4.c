dc4f40ac4f98cfb8155d4414140cc0db28f69915
VictorHugoMA <victor.alves1@ufu.br>
2021-04-03 14:34:36 -0300

    add

--- Listas_ED1/Lista9_ED1/ex4.c
+++ Listas_ED1/Lista9_ED1/ex4.c
@@ -13,21 +13,69 @@
         int ddd, num;
     };
 
-    struct cadastro{
+    typedef struct cadastro{
         char nome[20], cpf[15], sexo[2], est_civil[10], ident[15];
-        int salario;
+        float salario;
         struct endereco com;
         struct endereco cas;
         struct telefone pes;
         struct telefone res;
         struct data nasc;
         struct data ing;
-    };
+    }cadastro;
+
+    int menu(){
+        int op;
+        printf("\n\n=======Menu=======\n\n");
+        printf("1 - Listar todos os clientes\n");
+        printf("2 - Cadastrar/Sobrescrever novo cliente\n");
+        printf("3 - Listar quem nasceu depois de 1990\n");
+        printf("4 - Copiar de um cadastro para outro\n");
+        printf("0 - Sair\n\n");
+        printf("Escolha uma opcao valida: ");
+        scanf("%d", &op);
+
+        return op;
+    }
+    void imprimeCad(cadastro *cad){
+        int i;
+        for(i=0; i<5; i++){
+            printf("Nome %d: %s\n", i+1, *cad[i].nome);
+            printf("CPF %d: %d\n", i+1, *cad[i].cpf);
+            printf("Sexo %d: %s\n", i+1, *cad[i].sexo);
+            printf("Estado civil %d: %s\n", i+1, *cad[i].est_civil);
+            printf("Salario %d: %.2f\n", i+1, cad[i].salario);
+        }
+        printf("\n\n");
+
+    }
 
     int main(){
-        struct cadastro cad;
+        cadastro *cad;
+        int op;
 
-        cad.nasc.ano = 1990;
+        cad=(cadastro *)calloc(5, sizeof(cadastro));
 
-        printf("Ano %d\n", cad.nasc.ano);
+        cad[0].nasc.ano = 1990;
+        cad[0].salario=500;
+
+        printf("Ano %d\n", cad[0].nasc.ano);
+
+        do{
+            op=menu();
+            switch(op){
+            case 0:
+                break;
+            case 1:
+                imprimeCad(cad);
+                break;
+            
+            default:
+                printf("Opcao Invalida\n");
+            }
+        }
+        while(op!=0);
+
+        
+        free(cad);
     }
