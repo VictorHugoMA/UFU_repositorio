0b54f065e08c2c20fd5040cdd6b4dc8bc0ea7758
VictorHugoMA <victor.alves1@ufu.br>
2021-04-04 19:54:03 -0300

    add

--- Listas_ED1/Lista12_ED1/ex13.c
+++ Listas_ED1/Lista12_ED1/ex13.c
@@ -2,26 +2,31 @@
 //13 - Area do retangulo funcao
 
     typedef struct ponto{
-        int x, y;
+        float x, y;
     }ponto;
 
-    int area_calc(ponto p1, ponto p2){
-        int area;
+    float area_calc(ponto p1, ponto p2){
+        float area;
         area = (p2.x-p1.x)*(p1.y-p2.y);
         return area;
     }
-    int imprime_ponto (ponto p){
-        printf("(%d,%d)", p.x, p.y);
+    float imprime_ponto (ponto p){
+        printf("(%.2f, %.2f)", p.x, p.y);
     }
 
-    int main(){
-        int area;
+    float main(){
+        float area;
         ponto p1, p2;
 
-        scanf("%d", &p1.x);
-        scanf("%d", &p1.y);
-        scanf("%d", &p2.x);
-        scanf("%d", &p2.y);
+        printf("<<Calculo de Area>>\n");
+        printf("Digite a coordenada x do ponto 1: ");
+        scanf("%f", &p1.x);
+        printf("Digite a coordenada y do ponto 1: ");
+        scanf("%f", &p1.y);
+        printf("Digite a coordenada x do ponto 2: ");
+        scanf("%f", &p2.x);
+        printf("Digite a coordenada y do ponto 2: ");
+        scanf("%f", &p2.y);
 
         area = area_calc(p1, p2);
 
@@ -29,5 +34,5 @@
         imprime_ponto(p1);
         printf(" e ");
         imprime_ponto(p2);
-        printf(" eh %d\n",area);
+        printf(" eh %.2f", area);
     }
