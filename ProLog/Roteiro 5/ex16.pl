b3e57e59b0b90ff450c4fc4024806d821fbd94cf
VictorHugoMA <victor.alves1@ufu.br>
2021-04-08 14:06:03 -0300

    add

---
+++ ProLog/Roteiro 5/ex16.pl
@@ -0,0 +1,56 @@
+numero--> ns.
+numero--> nn.
+numero--> nc.
+numero--> nc2.
+numero--> nc3.
+
+numero--> nc, conj, ns.
+numero--> nc2, conj, ns.
+numero--> nc2, conj, nn.
+numero--> nc2, conj, nc.
+numero--> nc2, conj, nc, conj, ns.
+
+conj-->[e].
+
+ns-->[um].
+ns-->[dois].
+ns-->[tres].
+ns-->[quatro].
+ns-->[cinco].
+ns-->[seis].
+ns-->[sete].
+ns-->[oito].
+ns-->[nove].
+
+nn-->[dez].
+nn-->[onze].
+nn-->[doze].
+nn-->[treze].
+nn-->[quatorze].
+nn-->[quinze].
+nn-->[dezesseis].
+nn-->[dezessete].
+nn-->[dezoito].
+nn-->[dezenove].
+
+nc-->[vinte].
+nc-->[trinta].
+nc-->[quarenta].
+nc-->[cinquenta].
+nc-->[sessenta].
+nc-->[setenta].
+nc-->[oitenta].
+nc-->[noventa].
+
+nc2-->[cem].
+nc2-->[duzentos].
+nc2-->[trezentos].
+nc2-->[quatrocentos].
+nc2-->[quinhentos].
+nc2-->[seiscentos].
+nc2-->[setecentos].
+nc2-->[oitocentos].
+nc2-->[novecentos].
+
+nc3-->[mil].
+
