1bc8178a0cb3c63c21e186870fac176f23035a74
VictorHugoMA <victor.alves1@ufu.br>
2021-04-13 11:56:52 -0300

    add

---
+++ ProLog/Roteiro 6/ex1.pl
@@ -0,0 +1,44 @@
+/*
+? - 12 is 2*6.
+true.
+
+? - 14 =\= 2*6.
+true.
+
+? - 14 = 2*7.
+false.
+
+? - 14 == 2*7.
+false.
+
+? - 14 \== 2*7.
+true.
+
+? - 14 =:= 2*7.
+true.
+
+? - [1 ,2 ,3|[ d , e ]] == [1 ,2 ,3 ,d , e ].
+true.
+
+? - 2+3 == 3+2.
+false.
+
+? - 2+3 =:= 3+2.
+true.
+
+? - 7 -2 =\= 9 -2.
+true.
+
+? - p == 'p'.
+true.
+
+? - p =\= 'p'.
+ERROR: Arithmetic: `p/0' is not a function
+
+? - vicente == VAR .
+false.
+
+? - vicente = VAR , VAR == vicente .
+VAR = vicente.
+
+*/
---
+++ ProLog/Roteiro 6/ex5.pl
@@ -0,0 +1,15 @@
+:- op(300 , xfx , [sao, e_um]).
+:- op(300 , fx , gosta_de).
+:- op(200 , xfy , e).
+:- op(100 , fy , famoso).
+
+harry e ron e hermione sao amigos.
+harry e_um bruxo.
+harry e_um mago e gosta_de quadribol.
+dumbledore  e_um famoso famoso mago.
+
+/*
+Se adicionarmos algum e_um bruxo a primeira consulta funciona.
+O operador principal eh o famoso.
+*/
+
