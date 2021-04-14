ecefc645467fe2ebd5619e148380083433306351
VictorHugoMA <victor.alves1@ufu.br>
2021-04-14 11:51:52 -0300

    add

---
+++ ED1/Lista13/TadMat.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include "TadMat.h"
+
+    struct TadMat{
+        int nlinhas;
+        int ncolunas; 
+        double *dado;
+    };
+
+    TadMat *criar_mat(int nlin, int ncol){
+        TadMat *mat;
+
+        mat = malloc(sizeof(TadMat));
+        if(mat==NULL)
+            return NULL;
+        
+        mat->nlinhas = nlin;
+        mat->ncolunas = ncol;
+        mat->dado = malloc((nlin*ncol)*sizeof(double));
+
+        if(mat->dado==NULL){
+            free(mat);
+            return NULL;
+        }
+
+        return mat;
+    }
+
+    int free_mat(TadMat *mat){
+        if(mat==NULL)
+            return -1;
+        else{
+            free(mat->dado);
+            free(mat);
+            return 0;
+        }
+    }
---
+++ ED1/Lista13/TadMat.h
@@ -0,0 +1,4 @@
+typedef struct TadMat TadMat;
+
+TadMat *criar_mat(int nlin, int ncol);
+int free_mat(TadMat *mat);
