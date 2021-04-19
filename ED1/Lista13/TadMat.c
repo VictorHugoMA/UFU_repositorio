d37bfbe75e16e4e10af0eab6bd0c30b589962e1d
VictorHugoMA <victor.alves1@ufu.br>
2021-04-19 15:51:36 -0300

    add

--- ED1/Lista13/TadMat.c
+++ ED1/Lista13/TadMat.c
@@ -7,7 +7,8 @@
         int ncolunas; 
         double *dado;
     };
-
+    //Cria a matriz
+    //dado o numero de linhas e colunas
     TadMat *criar_mat(int nlin, int ncol){
         TadMat *mat;
 
@@ -27,6 +28,8 @@
         return mat;
     }
 
+    //Libera a memoria
+    //dado o ponteiro para a struct
     int free_mat(TadMat *mat){
         if(mat==NULL)
             return -1;
@@ -37,8 +40,8 @@
         }
     }
 
-    //escreve um valor na posicao desejada
-    //dado a struct *, a linha, a coluna e o valor 
+    //Escreve um valor na posicao desejada da matriz
+    //dado o ponteiro para a struct, a linha, a coluna e o valor 
     int escrever_mat(TadMat *mat, int nlin, int ncol, double val){
         if(mat==NULL)
             return -1;
@@ -52,13 +55,15 @@
         }
     }
 
-    int acessar_mat(TadMat *mat, int nlin, int ncol, double *val){
+    //Acessa uma posicao da matriz
+    //dado o ponteiro para a struct, a posicao(linha e coluna), e um ponteiro para double(aux) 
+    int acessar_mat(TadMat *mat, int lin, int col, double *val){
         if(mat==NULL)
             return -1;
 
         else{
             int pos;
-            pos = ncol * mat->nlinhas + nlin;
+            pos = col * mat->nlinhas + lin;
 
             *val=mat->dado[pos];
 
@@ -66,6 +71,8 @@
         }
     }
 
+    //Preenche uma matriz com valores aleatorios
+    //dado o ponteiro para a struct, o numero de linhas, numero de colunas e um valor maximo
     int preencher_mat(TadMat *mat, int nlin, int ncol, double max){
         if(mat==NULL)
             return -1;
@@ -79,6 +86,8 @@
         }
     }
 
+    //Soma duas matrizes
+    //dado dois ponterios para as structs, o numero de linhas e o numero de colunas
     int soma_mat(TadMat *m1, TadMat *m2, TadMat *sm, int nlin, int ncol){
         int i;
 
@@ -94,6 +103,8 @@
         }
     }
 
+    //Multiplica duas matrizes 
+    //dado o ponteiro para struct e o numero de linhas e colunas das duas matrizes a multiplicar o ponteiro para struct resultante 
     int mult_mat(TadMat *m1, int nlin1, int ncol1, TadMat *m2, int nlin2, int ncol2, TadMat *tm){
         if(m1==NULL || m2==NULL || tm==NULL || ncol1!=nlin2){
             return -1;
@@ -120,6 +131,8 @@
         }
     }
 
+    //Multiplica uma matriz por um numero
+    //dado o ponteiro para a struct, ponteiro para a struct resultado, o numero de linhas, colunas e o valor a ser multiplicado
     int multConst_mat(TadMat *m1, TadMat *tm, int nlin, int ncol, double num){
         if(m1==NULL){
             return -1;
@@ -133,6 +146,8 @@
         }
     }
 
+    //Traco ou soma da diagonal principal de uma matriz
+    //dado o ponteiro para a struct, o numero de linhas, colunas e o ponterio de double para receber a soma
     int traco_mat(TadMat *m1, int nlin, int ncol, double *tot){
 
         if(m1==NULL){
@@ -154,6 +169,8 @@
         }
     }
 
+    //Vetor com a soma das linahs
+    //dado o ponteiro para a struct, numero de linhas, colunas e o ponteiro para double
     int somaL_mat(TadMat *m1, int nlin, int ncol, double *sl){
         if(m1==NULL){
             return -1;
@@ -171,6 +188,8 @@
         }
     }
 
+    //Vetor com a soma das colunas
+    //dado o ponteiro para a struct, numero de linhas, colunas e o ponteiro para double
     int somaC_mat(TadMat *m1, int nlin, int ncol, double *sc){
         if(m1==NULL){
             return -1;
