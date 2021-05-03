904a16909890c5d27eef7983ca1fa7123b59f311
VictorHugoMA <victor.alves1@ufu.br>
2021-05-03 18:43:54 -0300

    formatado

--- ED1/Lista1/ex2.c
+++ ED1/Lista1/ex2.c
@@ -4,8 +4,8 @@
     int main(){
         float x;
 
-        printf("<<Conversor de Temperatura>>\n");
+        printf("<< Conversor de Temperatura >>\n");
         printf("Digite a temperatura (em Celsius): ");
         scanf("%f", &x);
-        printf("%.2f graus Celsius correspondem a %.2f Fahrenheit\n", x, x*(9.0/5.0)+32.0);
+        printf("%.0f graus Celsius correspondem a %.0f Fahrenheit\n", x, x*(9.0/5.0)+32.0);
     }
