798a01d9914aa67dd0dc9e8bba3890362dd66504
VictorHugoMA <victor.alves1@ufu.br>
2021-05-24 13:50:48 -0300

    add

---
+++ ProLog/Trabalho/Clinica_Dentaria.pl
@@ -0,0 +1,108 @@
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/html_write)).
+
+servidor(Porta) :-
+http_server(http_dispatch, [port(Porta)]).
+
+:- http_handler(root(.), home , []).
+
+
+/* html_requires está aqui */
+:- use_module(library(http/html_head)).
+
+/* serve_files_in_directory está aqui */
+:- use_module(library(http/http_server_files)).
+
+/* Localização dos diretórios no sistema de arquivos */
+:- multifile user:file_search_path/2.
+
+user:file_search_path(dir_css, 'css').
+user:file_search_path(dir_js, 'js').
+
+/* Liga as rotas aos respectivos diretórios */
+:-http_handler(css(.), serve_files_in_directory(dir_css), [prefix]).
+:-http_handler(js(.), serve_files_in_directory(dir_js), [prefix]).
+
+% Liga a rota ao tratador
+:- http_handler(root(.), home , []).
+:- http_handler(root(tabela1), tabela1 , []).
+:- http_handler(root(tabela2), tabela2 , []).
+
+:- multifile user:body//2.
+
+user:body(bootstrap, Corpo) -->
+    html(body([ \html_post(head,
+        [ meta([name(viewport),
+        content('width=device-width, initial-scale=1')])]),
+        \html_root_attribute(lang,'pt-br'),
+        \html_requires(css('bootstrap.min.css')),
+
+        Corpo,
+
+        script([ src('js/bootstrap.bundle.min.js'),
+        type('text/javascript')], [])
+    ])).
+
+
+
+home(_Pedido) :-
+    reply_html_page(
+    bootstrap,
+    [ title('Clinica Dentaria')],
+    [ div(class(container),
+        [ h1('Sistema de Gestao para Clinica Dentaria'),
+            nav(class(['nav', 'flex-column']),
+                [ p(\link_tabela(1)),
+                  p(\link_tabela(2)) ])
+        ])
+    ]).
+
+link_tabela(N) -->
+    html(a([ class(['nav-link']),
+        href('/tabela~d' -N)],
+        'Tabela ~d' -N)).
+
+
+tabela1(_Pedido) :-
+    reply_html_page(
+    bootstrap,
+    [ title('Exemplo 1')],
+    [ div(class(container),
+        [ \html_requires(css('estilo.css')),
+            h2(class("my-5 text-center"),
+                'Exemplo 1 com Bootstrap 5 e SWI-Prolog'),
+            div(class("container my-5"),
+                div(class(row),
+                    [div(class('col-md-8'),
+                        'Conteudo Principal'),
+                    div(class('col-md-4'),
+                        'Barra Lateral')])),
+            \retorna_home ])]).
+
+retorna_home -->
+    html(div(class(row),
+        a([ class(['btn', 'btn-primary']), href('/')],
+            'Voltar para o início'))).
+
+tabela2(_Pedido) :-
+    reply_html_page(
+        bootstrap,
+        [ title('Exemplo 2')],
+        [ div(class(container),
+            [ \html_requires(css('estilo.css')),
+                h2(class("my-5 text-center"),
+                    'Exemplo 2 com Bootstrap 5 e SWI-Prolog'),
+                div(class('container my-5'),
+                    div(class(row),
+                        [div(class('col-md-8 text-center'),
+                            ['Cabecalho',
+                        div(class="row text-center",
+                            [div(class="col-md-6",
+                                [ 'Artigo 1', p('Paragrafo 1.')]),
+                            div(class="col-md-6",
+                                [ 'Artigo 2', p('Paragrafo 2.')]),
+                            span('Rodape')])]),
+                    div(class('col-md-4'),
+                        'Barra Lateral')])),
+                \retorna_home ])]).
