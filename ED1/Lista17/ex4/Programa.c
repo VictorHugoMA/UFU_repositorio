6935e76a58c904e31b62351ebc055a147d48c2b4
VictorHugoMA <victor.alves1@ufu.br>
2021-05-26 14:35:38 -0300

    add

---
+++ ED1/Lista17/ex4/Programa.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"TStack.h"
+
+    int main(){
+        int aux;
+        char str[7]= "([()])";
+        TStack *t;
+
+        t = stack_create; //cria a stack
+
+        for(int i=0; i<8; i++){
+            stack_push(t, str[i]); //insere na stack
+        }
+
+        aux=bemFormada(t); //informa se a string tem os parenteses e colchetes bem formados
+
+        if(aux==1)
+            printf("Sequencia bem formada");
+        else
+            printf("Sequencia nao eh bem formada");
+
+
+        return 0;
+    }
