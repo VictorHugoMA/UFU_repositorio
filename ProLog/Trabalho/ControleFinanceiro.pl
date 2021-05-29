806f78f0afa61a5f9936ed4411e216d53f9ab472
VictorHugoMA <victor.alves1@ufu.br>
2021-05-28 23:17:43 -0300

    add

--- ProLog/Trabalho/ControleFinanceiro.pl
+++ ProLog/Trabalho/ControleFinanceiro.pl
@@ -66,7 +66,43 @@ link_formapagamento(1) -->
         'Forma de Pagamento')).
 
 
-:- ensure_loaded([tesouraria, formapagamento]).
+tesouraria(_Pedido):-
+    reply_html_page( 
+    bootstrap,
+    [ title('Tesouraria')],
+    [ div(class(container),
+        [ \html_requires(css('estilo.css')),
+            h2(class("my-5 text-center"),
+                'Tesouraria'),
+            \campo(id_tesouraria, 'ID Tesouraria', number),
+            \campo(id_empresa, 'ID Empresa', number),
+            \campo(id_cliente, 'ID Cliente', number),
+            \campo(id_planoContas, 'ID Plano de Contas', number),
+            \campo(id_fornecedores, 'ID Fornecedores', number),
+            \campo(formapagamento_tes, 'Formas de Pagamento', text),
+            \campo(valor_tes, 'Valor', text),
+            \campo(numero_tes, 'Numero', text),
+            \campo(data_emissao_tes, 'Data de Emissao', date),
+            \campo(data_venc_tes, 'Data de Vencimento', date),
+            \campo(data_disp_tes, 'Data de Disponibilidade', date),
+            
+            p(button([ class('btn btn-primary'), type(submit)], 'Enviar')),
+            \retorna_home ])]).
+
+
+formapagamento(_Pedido):-
+    reply_html_page( 
+    bootstrap,
+    [ title('Formas de Pagamentos')],
+    [ div(class(container),
+        [ \html_requires(css('estilo.css')),
+            h2(class("my-5 text-center"),
+                'Formas de Pagamento'),
+            \campo(id_formapagamento, 'ID Forma de Pagamento', number),
+            \campo(descr_formapagento, 'Descricao', text),
+    
+            p(button([ class('btn btn-primary'), type(submit)], 'Enviar')),
+            \retorna_home ])]).
 
 retorna_home -->
     html(div(class(row),
