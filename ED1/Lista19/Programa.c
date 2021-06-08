5d75800c9030c0d62934bc4747533012773a408d
VictorHugoMA <victor.alves1@ufu.br>
2021-06-08 12:35:02 -0300

    add TCircList

---
+++ ED1/Lista19/Programa.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include"TCircList.h"
+
+    int main(){
+        aluno a[10]={
+        {120, "Victor", 10, 10, 10},
+        {110, "Pedro", 10, 10, 10},
+        {130, "Joao", 7, 8, 10},
+        {105, "Maria", 7.5, 9.1, 10},
+        {150, "Guilherme", 6.5, 9.0, 8},
+        {100, "Alisson", 10, 10, 10},
+        {170, "Felipe", 10, 10, 10},
+        {200, "Carolina", 7, 8, 10},
+        {115, "Jorge", 7.5, 9.1, 10},
+        {195, "Ana", 6.5, 9.0, 8}
+        };
+        int aux;
+
+        CircList *c;
+
+        c = list_creat();
+
+        aux = list_push_back(c, a[0]);
+        printf("%d\n", aux);
+        aux = list_push_front(c, a[1]);
+        printf("%d\n", aux);
+        aux = list_push_front(c, a[2]);
+        printf("%d\n", aux);
+        aux = list_push_back(c, a[3]);
+        printf("%d\n", aux);
+
+        list_print(c);
+
+        printf("\nRemovendo um elemento:\n");
+        list_pop_front(c);
+        list_print(c);
+
+        list_free(c);
+        return 0;
+    }
---
+++ ED1/Lista19/TCircList.c
@@ -0,0 +1,126 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"TCircList.h"
+
+typedef struct CLNode CLNode;
+
+struct CLNode{
+    CLNode *next;
+    aluno data;
+};
+
+struct CircList{
+    CLNode *end;
+};
+
+
+CircList *list_creat(){
+    CircList *c;
+
+    c = malloc(sizeof(CircList));
+
+    if(c!=NULL){
+        c->end = NULL;
+    }
+    return c;
+}
+
+int list_free(CircList *c){
+    if(c==NULL){
+        return INVALID_NULL_POINTER;
+    }
+    CLNode *aux, *prox;
+    aux = c->end->next;
+
+    while(aux->next!=c->end){
+        prox = aux->next;
+        free(aux);
+        aux = prox;
+    }
+    free(c->end);
+
+    return SUCCESS;
+}   
+
+int list_push_front(CircList *c, aluno a){
+    if(c==NULL){
+        return INVALID_NULL_POINTER;
+    }
+    CLNode *n;
+
+    n = malloc(sizeof(CLNode));
+
+    if(n==NULL){
+        return OUT_OF_MEMORY;
+    }
+    if(c->end==NULL){
+        c->end=n;
+    }
+    n->data = a;
+    n->next = c->end->next;
+    c->end->next=n;
+
+    return SUCCESS;
+}
+
+int list_push_back(CircList *c, aluno a){
+    if(c==NULL){
+        return INVALID_NULL_POINTER;
+    }
+    CLNode *n;
+
+    n = malloc(sizeof(CLNode));
+
+    if(n==NULL){
+        return OUT_OF_MEMORY;
+    }
+    if(c->end==NULL){
+        c->end=n;
+    }
+    n->data = a;
+    n->next = c->end->next;
+    c->end->next = n;
+    c->end = n;
+
+    return SUCCESS;
+}
+
+int list_pop_front(CircList *c){
+    if(c==NULL){
+        return INVALID_NULL_POINTER;
+    }
+    if(c->end==NULL){
+        return ELEM_NOT_FOUND;
+    }
+    CLNode *aux;
+
+    aux = c->end->next;
+    c->end->next = c->end->next->next;
+    free(aux);
+
+    return SUCCESS;
+}
+
+int list_print(CircList *c){
+    if(c==NULL){
+        return INVALID_NULL_POINTER;
+    }
+    CLNode *aux, *prox;
+    aux = c->end;
+
+    while(aux->next!=c->end){
+        aux = aux->next;
+
+        printf("\n------------------\n");
+        printf("Matricula: %d\n", aux->data.matricula);
+        printf("Nome: %s\n", aux->data.nome);
+        printf("Notas: %.1f; %.1f; %.1f;\n", aux->data.n1, aux->data.n2, aux->data.n3);
+        
+    }
+    printf("\n------------------\n");
+    printf("Matricula: %d\n", c->end->data.matricula);
+    printf("Nome: %s\n", c->end->data.nome);
+    printf("Notas: %.1f; %.1f; %.1f;\n", c->end->data.n1, c->end->data.n2, c->end->data.n3);
+
+    return SUCCESS;
+}
