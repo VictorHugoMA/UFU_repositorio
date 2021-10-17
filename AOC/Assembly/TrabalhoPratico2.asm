d689646c7c76d13c8a82e06a68bf7e33d0045648
VictorHugoMA <victor.alves1@ufu.br>
2021-10-17 11:43:41 -0300

    add enunciado trabalho 2 AOC

---
+++ AOC/Assembly/TrabalhoPratico2.asm
@@ -0,0 +1,8 @@
+#Escreva um programa em assembly mips que salva os números de 1 a 10 na memória.
+#A seguir leia os números da memória, calcule seu quadrado e salve novamente na memória
+
+.data
+
+
+
+.text
