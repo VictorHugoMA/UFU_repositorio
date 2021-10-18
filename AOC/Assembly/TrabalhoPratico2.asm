24ffdc86cc00c78ac92d028577ba51d77b00fbda
VictorHugoMA <victor.alves1@ufu.br>
2021-10-18 10:36:50 -0300

    resolucao assembly trabalho pratico 2 AOC

--- AOC/Assembly/TrabalhoPratico2.asm
+++ AOC/Assembly/TrabalhoPratico2.asm
@@ -2,7 +2,42 @@
 #A seguir leia os números da memória, calcule seu quadrado e salve novamente na memória
 
 .data
-
+	.align 2
+	array: .space 4000
 
 
 .text
+
+	la	$s7, array
+	addiu 	$s0, $zero, 1		# contador
+	addiu 	$s5, $zero, 11 		# maximo
+
+
+FOR1:	slt	$t0, $s0, $s5	  	# s0 <11 -> t0 = 1	
+	beq	$t0, $zero, SAI1
+	
+	sw	$s0, 0($s7)       	# MEM[$s7+0] <- $s0
+	
+	addiu	$s7,$s7, 4        	# apontar pro proximo elemento do array
+	addiu	$s0, $s0, 1		# +1 no contador
+	j	FOR1
+
+SAI1:	la $s7, array			# volta para a primeira pos do array
+	addiu 	$s0, $zero, 1		# contador
+	addiu 	$s5, $zero, 11 		# maximo
+
+
+FOR2:	slt	$t0, $s0, $s5	  	# s0 <11 -> t0 = 1	
+	beq	$t0, $zero, SAI2
+	
+	lw	$t1, 0($s7) 
+	mul	$s6, $t1, $t1		# calcula o quadrado
+	sw	$s6, 0($s7)    	
+	
+	addiu	$s7,$s7, 4        	# apontar pro proximo elemento do array
+	addiu	$s0, $s0, 1		# +1 no contador
+	j	FOR2
+
+SAI2: 
+	
+	
