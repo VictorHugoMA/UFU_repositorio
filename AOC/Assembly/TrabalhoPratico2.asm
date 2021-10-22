3a88df595ec2459fad50c6fad795f6e193e42786
VictorHugoMA <victor.alves1@ufu.br>
2021-10-22 11:27:17 -0300

    modificacao para instrucao basica assembly

--- AOC/Assembly/TrabalhoPratico2.asm
+++ AOC/Assembly/TrabalhoPratico2.asm
@@ -2,10 +2,8 @@
 #A seguir leia os números da memória, calcule seu quadrado e salve novamente na memória
 
 .text
-
-	#la	$s7, array
-	lui $at, 4097
-	ori $s7, $at, 0
+	lui 	$at, 4097
+	ori 	$s7, $at, 0
 	addiu 	$s0, $zero, 1		# contador
 	addiu 	$s5, $zero, 11 		# maximo
 
@@ -19,9 +17,8 @@ FOR1:	slt	$t0, $s0, $s5	  	# s0 <11 -> t0 = 1
 	addiu	$s0, $s0, 1		# +1 no contador
 	j	FOR1
 
-SAI1:	#la $s7, array			# volta para a primeira pos do array
-	lui $at, 4097
-	ori $s7, $at, 0
+SAI1:	lui 	$at, 4097			
+	ori 	$s7, $at, 0		#voltar para primeira posicao do array
 	addiu 	$s0, $zero, 1		# contador
 	addiu 	$s5, $zero, 11 		# maximo
 
@@ -29,8 +26,9 @@ SAI1:	#la $s7, array			# volta para a primeira pos do array
 FOR2:	slt	$t0, $s0, $s5	  	# s0 <11 -> t0 = 1	
 	beq	$t0, $zero, SAI2
 	
-	lw	$t1, 0($s7) 
-	mul	$s6, $t1, $t1		# calcula o quadrado
+	lw	$t1, 0($s7)
+	mult	$t1, $t1		# calcula o quadrado
+	mflo	$s6
 	sw	$s6, 0($s7)    	
 	
 	addiu	$s7,$s7, 4        	# apontar pro proximo elemento do array
