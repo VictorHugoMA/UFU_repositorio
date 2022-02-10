a6fbb24e603acc066f82f3180753e07a628250dd
VictorHugoMA <victor.alves1@ufu.br>
2022-02-10 13:59:32 -0300

    att tabela de criacao

--- BD1/Trabalho/criarTabelas.sql
+++ BD1/Trabalho/criarTabelas.sql
@@ -1,7 +1,8 @@
 CREATE SCHEMA petshop;
 
 CREATE TABLE Compra(
-    idCompra Serial,
+    idCompra Serial NOT NULL PRIMARY KEY,
+    Fornecedor_idFornecedor Serial,
     dataCompra date,
     precoCompra float,
     produto varchar(45),
@@ -9,7 +10,7 @@ CREATE TABLE Compra(
 );
 
 CREATE TABLE Fornecedor(
-    idFornecedor Serial,
+    idFornecedor Serial NOT NULL PRIMARY KEY,
     nome varchar(45),
     cnpj varchar(45),
     tefone varchar(45),
@@ -19,3 +20,19 @@ CREATE TABLE Fornecedor(
     numeroEndereco int,
     estado varchar(20)
 );
+
+CREATE TABLE Produto_has_Compra(
+    Produto_idProduto Serial NOT NULL,
+    Compra_idCompra Serial NOT NULL ,
+    preco float,
+    qtd int,
+    CONSTRAINT fk_phc PRIMARY KEY (Produto_idProduto, Compra_idCompra)
+);
+
+ALTER TABLE Compra
+    add CONSTRAINT fornecedorFK foreign key (Fornecedor_idFornecedor) references Fornecedor(idFornecedor);
+
+
+ALTER TABLE Produto_has_Compra
+    add CONSTRAINT produtoFK foreign key (Produto_idProduto) references Produto(idProduto),
+    add CONSTRAINT CompraFK foreign key (Compra_idCompra) references Compra(idCompra);
