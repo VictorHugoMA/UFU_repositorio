3cd7e92f2a17eda1b04a5e32d94eaba8d498b4a3
VictorHugoMA <victor.alves1@ufu.br>
2022-02-14 14:20:06 -0300

    add ex4 Trabalho 7 PPI

---
+++ PPI/trabalhos/trabalho7/ex4/buscaMedico.php
@@ -0,0 +1,43 @@
+<?php
+require "../sql/conexaoMysql.php";
+
+class Medico
+{
+  public $nome;
+  public $telefone;
+  public $especialidade;
+
+  function __construct($nome, $telefone, $especialidade)
+  {
+    $this->nome = $nome;
+    $this->telefone = $telefone; 
+    $this->especialidade = $especialidade;
+  }
+}
+
+$pdo = mysqlConnect();
+
+$especialidade = $_GET['especialidade'] ?? '';
+
+try {
+    $sql = <<<SQL
+    SELECT nome_medico, telefone_medico
+    FROM medico WHERE especialidade_medico = ?
+    SQL;
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$especialidade]);
+
+} 
+catch (Exception $e) {
+    exit('Ocorreu uma falha: ' . $e->getMessage());
+}
+
+while($row = $stmt->fetch()){
+    $medicos[] = new Medico($row['nome_medico'], $row['telefone_medico'], $row['especialidade_medico']);
+}
+
+
+echo json_encode($medicos);
+
+?>
