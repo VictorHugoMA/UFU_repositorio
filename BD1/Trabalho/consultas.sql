7fdd59f51ed29f9adfc4b010b346304545973a04
VictorHugoMA <victor.alves1@ufu.br>
2022-02-18 15:58:05 -0300

    add consultas sql trabalho BD1

---
+++ BD1/Trabalho/consultas.sql
@@ -0,0 +1,38 @@
+SET search_path TO petshop;
+
+select * from cliente c;
+select * from pet p;
+select * from profissional pr;
+select * from fornecedor f;
+select * from produto pr;
+select * from vacina v;
+select * from servico s;
+select * from compra co;
+select * from agenda a;
+select * from agenda_has_servico ahs;
+select * from ordemservico_has_produto ohp ;
+select * from produto_has_compra phc;
+
+select c.nome, p.nome, p.especie 
+	from cliente c inner join pet p 
+	on c.idcliente = p.cliente_idcliente;
+
+select * from pet p 
+	where especie  = 'Cachorro';
+
+select p.idprofissional, p.nome, p.funcao, ohs.ordemservico_idordemservico, ohs.servico_idservico
+	from profissional p inner join ordemservico_has_servico ohs
+	on p.idprofissional = ohs.profissional_idprofissional;
+
+select * from produto p , estoque e 
+	where p.idproduto = e.produto_idproduto ;
+
+select * from agenda a 
+	where cliente_idcliente = 1;
+
+select a.idagenda, a.pet_idpet, a.pet_idpet, a.datainicio, ahs.hora, ahs.servico_idservico
+	from agenda a inner join agenda_has_servico ahs
+	on a.idagenda = ahs.agenda_idagenda;
+
+select * from pet_vacina pv
+	where vacina_idvacina = 1;
