223b77298b358526d64a0704a8be434c77985bb7
Victor Hugo Alves <80076522+VictorHugoMA@users.noreply.github.com>
2022-05-17 21:43:18 -0300

    Update Lista-Consultas2.sql

--- BD2/Exercicios/Lista-Consultas2.sql
+++ BD2/Exercicios/Lista-Consultas2.sql
@@ -49,42 +49,42 @@ SELECT DISTINCT nome_cliente FROM emprestimo WHERE nome_agencia='PUC'
 
 SELECT nome_cliente
 FROM Cliente
-WHERE 'Belo Horizonte';
+WHERE cidade_cliente = 'Belo Horizonte';
 
 --08-Nomes de clientes que tem 'Santos' no nome
 
-SELECT nome_cliente
+SELECT nome_cliente FROM cliente
 WHERE nome_cliente LIKE '%Santos%';
 
 --09-Nomes de clientes que tem terminam com 'Souza' ou 'Sousa'
 
 SELECT nome_cliente
 FROM cliente
-WHERE nome_cliente = '%Sou_a';
+WHERE nome_cliente LIKE '%Sou_a';
 
 --10-Total de depósitos para cada nome de cliente
 
-SELECT nome_cliente, sum(1)
-FROM deposito GROUP BY nome_cliente;
+SELECT nome_cliente, sum(saldo_deposito)
+FROM deposito GROUP BY nome_cliente, saldo_deposito;
 
 --11-Nomes de clientes e depósitos que estão entre 
 --R$ 3.000,00 e R$ 4.000,00
 
 SELECT nome_cliente, saldo_deposito
 FROM deposito 
-WHERE saldo_deposito >= 4000 
-AND  saldo_deposito <= 4000
+WHERE saldo_deposito >= 3000 
+AND  saldo_deposito <= 4000;
 
 SELECT nome_cliente, saldo_deposito
 FROM deposito 
-WHERE saldo_deposito BETWEEN 3000 
+WHERE saldo_deposito BETWEEN 3000 AND 4000;
 
 --12-Nomes de clientes e a soma de depósitos de cada nome, 
 --quando a soma for maior que R$ 5.000,00
 
-SELECT nome_cliente, sum(saldo_deposito)
+SELECT nome_cliente, sum(saldo_deposito) 
 FROM deposito GROUP BY nome_cliente
-HAVING sum() > 5000
+HAVING sum(saldo_deposito) > 5000;
 
 --13-Nomes de clientes e a soma de depósitos de cada nome,
 --quando a soma estiver entre R$ 1.000,00 e R$ 4.000,00,
@@ -92,8 +92,9 @@ HAVING sum() > 5000
 
 SELECT nome_cliente, sum(saldo_deposito)
 FROM deposito 
+GROUP BY nome_cliente
 HAVING sum(saldo_deposito) BETWEEN 1000 AND 4000
-ORDER BY sum(saldo_deposito)
+ORDER BY sum(saldo_deposito);
 
 
 --14-Nomes de clientes e a soma de depósitos de cada nome,
@@ -101,9 +102,10 @@ ORDER BY sum(saldo_deposito)
 --em ordem pela soma
 
 SELECT nome_cliente, sum(saldo_deposito)
-FROM deposito GROUP BY nome_cliente
-HAVING sum() > avg()
-ORDER BY sum(saldo_deposito)
+FROM deposito 
+GROUP BY nome_cliente
+HAVING sum(saldo_deposito) > avg(saldo_deposito)
+ORDER BY sum(saldo_deposito);
 
 --15-Faça uma listagem da soma de depósitos feitos
 --por cada cliente em cada agência. Ordene o resultado
@@ -111,8 +113,8 @@ ORDER BY sum(saldo_deposito)
 
 SELECT nome_agencia, nome_cliente, sum(saldo_deposito)
 FROM deposito 
-GROUP BY nome_cliente
-ORDER BY nome_cliente, nome_agencia
+GROUP BY nome_cliente, nome_agencia
+ORDER BY nome_cliente, nome_agencia;
 
 --16- Selecione os nomes dos clientes e a soma de depósitos
 --feitos por eles em cada agência que foram maiores que todos
@@ -129,7 +131,7 @@ WHERE  GROUP BY nome_agencia
 --Conjunto1 HAVING sum > ALL Conjunto2
 SELECT nome_cliente, nome_agencia, sum(saldo_deposito) 
 FROM deposito GROUP BY nome_cliente, nome_agencia 
-HAVING sum() > ALL
+HAVING sum(saldo_deposito) > ALL
 (SELECT sum(saldo_deposito) FROM deposito 
-WHERE  GROUP BY nome_agencia);
+WHERE nome_agencia = 'Pampulha' GROUP BY nome_agencia);
 
