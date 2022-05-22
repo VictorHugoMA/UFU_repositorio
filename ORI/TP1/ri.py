1f81c782aafaf99d87b38c8960bbf215a65813de
VictorHugoMA <victor.alves1@ufu.br>
2022-05-21 21:34:39 -0300

    add exercicio RI

---
+++ ORI/TP1/ri.py
@@ -0,0 +1,57 @@
+import string
+import unidecode
+
+def separaArquivo(nomeArquivo):
+  texto = open(nomeArquivo, 'r')
+  texto = texto.read()
+  
+  texto = unidecode.unidecode(texto)
+  pontuacao = string.punctuation
+  for i in pontuacao:
+    texto = texto.replace(i,"")
+  
+  termos = texto.lower().split()
+  termos.sort()
+  termos = list(dict.fromkeys(termos))
+
+  return termos
+  
+def comparaTermos(vocabulario, documento):
+  bagOfWords = []
+  for i in vocabulario:
+    if i in documento:
+      bagOfWords.append(1)
+    else:
+      bagOfWords.append(0)
+
+  return bagOfWords
+
+def comparaInputArquivos():
+  nomeDocumento=''
+  print('DIGITE ! PARA FINALIZAR')
+  while(1):
+    bagOfWords = []
+    nomeDocumento=input('\nDigite o nome do documento: ')
+    
+    if(nomeDocumento=='!'):
+      break
+    
+    documento = separaArquivo(nomeDocumento)
+    print(f'\nDocumento: {documento}\n')
+    bagOfWords = comparaTermos(vocabulario, documento)
+    print(f'Bag of Words: {bagOfWords}')
+
+def comparaListaArquivos(lista):
+  for i in lista:
+    bagOfWords = []
+    documento = separaArquivo(i)
+    print(f'\nDocumento: {documento}\n')
+    bagOfWords = comparaTermos(vocabulario, documento)
+    print(f'Bag of Words: {bagOfWords}')
+  
+
+
+vocabulario = separaArquivo('vocabulario.txt')
+print(f'Vocabulário: {vocabulario}\n')
+lista = ['documento01.txt', 'Hino.txt']
+comparaListaArquivos(lista)
