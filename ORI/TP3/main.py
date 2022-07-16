2ebf251cfc847cb4b3c1a77fa64c198c483995b4
VictorHugoMA <victor.alves1@ufu.br>
2022-07-16 11:58:49 -0300

    att main

--- ORI/TP3/main.py
+++ ORI/TP3/main.py
@@ -1,7 +1,13 @@
 import ModeloVetorial
 
-listaDoc = [ 'd1.txt', 'd2.txt', 'd3.txt', 'd4.txt'] 
-mv = ModeloVetorial.ModeloVetorial(listaDoc)
+listaDoc = [ 'd1.txt', 'd2.txt', 'd3.txt', 'd4.txt']
+
+listaDoc2 = [ 'hinoAmericaMG.txt', 'hinoAthletico.txt', 'hinoAtleticoGO.txt', 'hinoAtleticoMG.txt', 'hinoAvai.txt',
+            'hinoBotafogo.txt', 'hinoBragantino.txt', 'hinoCeara.txt', 'hinoCorinthians.txt', 'hinoCoritiba.txt',
+            'hinoCuiaba.txt', 'hinoFlamengo.txt', 'hinoFluminense.txt', 'hinoFortaleza.txt', 'hinoGoias.txt',
+            'hinoInternacional.txt', 'hinoJuventude.txt','hinoPalmeiras.txt', 'hinoSantos.txt', 'hinoSaoPaulo.txt'] 
+
+mv = ModeloVetorial.ModeloVetorial(listaDoc2)
 
 mv.inputCalculaConsulta()
 
